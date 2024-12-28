635bd171a9d000536402b620fd0456e7e23ca7d4
dddddjent <1306043330a@gmail.com>
2024-12-28 18:23:07 -0500

    derive Object from Resource

--- function/type/object.h
+++ function/type/object.h
@@ -3,12 +3,13 @@
 #include "core/config/config.h"
 #include "core/vulkan/descriptor_manager.h"
 #include "core/vulkan/type/buffer.h"
+#include "function/resource_manager/resource.h"
 
 #ifdef _WIN64
 #include <Windows.h>
 #endif
 
-struct Object {
+struct Object : public Resource {
     struct Param {
         Vk::DescriptorHandle material;
     };
@@ -25,6 +26,7 @@ struct Object {
 #else
     int getVkVertexMemHandle();
 #endif
-    void destroy();
+    virtual constexpr std::string type() const override { return "Object"; }
+    virtual void destroy() override;
     static Object fromConfiguration(ObjectConfiguration& config);
 };
